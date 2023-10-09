98e7ada7570db47cdbfc5569ccef587509ddc627
Ann Villegas <144677995+pastepastepaste@users.noreply.github.com>
2023-10-10 02:23:12 +0800

    Add files via upload

---
+++ frmConfirmation.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Organization_Profile
+{
+    partial class frmConfirmation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lblProgram = new Label();
+            lblGender = new Label();
+            lblContactNo = new Label();
+            lblBirthday = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            lblStudentNo = new Label();
+            lblName = new Label();
+            lblAge = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            btnSubmit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(52, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 18);
+            label1.TabIndex = 0;
+            label1.Text = "Student no. :";
+            // 
+            // lblProgram
+            // 
+            lblProgram.AutoSize = true;
+            lblProgram.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblProgram.ForeColor = Color.FromArgb(107, 179, 107);
+            lblProgram.Location = new Point(160, 154);
+            lblProgram.Name = "lblProgram";
+            lblProgram.Size = new Size(44, 18);
+            lblProgram.TabIndex = 1;
+            lblProgram.Text = "label2";
+            // 
+            // lblGender
+            // 
+            lblGender.AutoSize = true;
+            lblGender.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblGender.ForeColor = Color.FromArgb(107, 179, 107);
+            lblGender.Location = new Point(160, 298);
+            lblGender.Name = "lblGender";
+            lblGender.Size = new Size(44, 18);
+            lblGender.TabIndex = 2;
+            lblGender.Text = "label3";
+            // 
+            // lblContactNo
+            // 
+            lblContactNo.AutoSize = true;
+            lblContactNo.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblContactNo.ForeColor = Color.FromArgb(107, 179, 107);
+            lblContactNo.Location = new Point(160, 347);
+            lblContactNo.Name = "lblContactNo";
+            lblContactNo.Size = new Size(45, 18);
+            lblContactNo.TabIndex = 3;
+            lblContactNo.Text = "label4";
+            // 
+            // lblBirthday
+            // 
+            lblBirthday.AutoSize = true;
+            lblBirthday.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblBirthday.ForeColor = Color.FromArgb(107, 179, 107);
+            lblBirthday.Location = new Point(160, 250);
+            lblBirthday.Name = "lblBirthday";
+            lblBirthday.Size = new Size(44, 18);
+            lblBirthday.TabIndex = 4;
+            lblBirthday.Text = "label5";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(52, 344);
+            label6.Name = "label6";
+            label6.Size = new Size(86, 18);
+            label6.TabIndex = 5;
+            label6.Text = "Contact No. : ";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(52, 250);
+            label7.Name = "label7";
+            label7.Size = new Size(67, 18);
+            label7.TabIndex = 6;
+            label7.Text = "Birthday : ";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(52, 203);
+            label8.Name = "label8";
+            label8.Size = new Size(37, 18);
+            label8.TabIndex = 7;
+            label8.Text = "Age :";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(52, 154);
+            label9.Name = "label9";
+            label9.Size = new Size(67, 18);
+            label9.TabIndex = 8;
+            label9.Text = "Program :";
+            // 
+            // lblStudentNo
+            // 
+            lblStudentNo.AutoSize = true;
+            lblStudentNo.BackColor = Color.FromArgb(204, 255, 204);
+            lblStudentNo.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblStudentNo.ForeColor = Color.FromArgb(107, 179, 107);
+            lblStudentNo.Location = new Point(160, 58);
+            lblStudentNo.Name = "lblStudentNo";
+            lblStudentNo.Size = new Size(49, 18);
+            lblStudentNo.TabIndex = 9;
+            lblStudentNo.Text = "label10";
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblName.ForeColor = Color.FromArgb(107, 179, 107);
+            lblName.Location = new Point(160, 109);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(47, 18);
+            lblName.TabIndex = 10;
+            lblName.Text = "label11";
+            // 
+            // lblAge
+            // 
+            lblAge.AutoSize = true;
+            lblAge.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblAge.ForeColor = Color.FromArgb(107, 179, 107);
+            lblAge.Location = new Point(160, 203);
+            lblAge.Name = "lblAge";
+            lblAge.Size = new Size(49, 18);
+            lblAge.TabIndex = 11;
+            lblAge.Text = "label12";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label13.Location = new Point(52, 298);
+            label13.Name = "label13";
+            label13.Size = new Size(61, 18);
+            label13.TabIndex = 12;
+            label13.Text = "Gender : ";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label14.Location = new Point(52, 106);
+            label14.Name = "label14";
+            label14.Size = new Size(53, 18);
+            label14.TabIndex = 13;
+            label14.Text = "Name : ";
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.BackColor = Color.FromArgb(153, 255, 153);
+            btnSubmit.Font = new Font("Bahnschrift SemiCondensed", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSubmit.Location = new Point(142, 393);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(98, 34);
+            btnSubmit.TabIndex = 14;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = false;
+            // 
+            // frmConfirmation
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(204, 255, 204);
+            ClientSize = new Size(354, 450);
+            Controls.Add(btnSubmit);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(lblAge);
+            Controls.Add(lblName);
+            Controls.Add(lblStudentNo);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(lblBirthday);
+            Controls.Add(lblContactNo);
+            Controls.Add(lblGender);
+            Controls.Add(lblProgram);
+            Controls.Add(label1);
+            Name = "frmConfirmation";
+            Text = "frmConfirmation";
+            Load += frmConfirmation_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblProgram;
+        private Label lblGender;
+        private Label lblContactNo;
+        private Label lblBirthday;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label lblStudentNo;
+        private Label lblName;
+        private Label lblAge;
+        private Label label13;
+        private Label label14;
+        private Button btnSubmit;
+    }
+}
